a6a590f657f40ab535012cd25c54b0f828a0a059
CristobalSoto <csotov2010@alu.uct.cl>
2014-06-16 03:33:10 -0400

    clases buses y choferes
    Solo agregue las clases ya me faltaria modificar las clases principales
    y los metodos

---
+++ SmarTravel_Final/SmarTravel_Final/ChoferDatosEspecificos.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+using System.Data.Sql;
+using System.Data;
+using System.IO;
+
+
+namespace SmarTravel_Final
+{
+    /// <summary>
+    /// Lógica de interacción para ChoferDatosEspecificos.xaml
+    /// </summary>
+    public partial class ChoferDatosEspecificos : Window
+    {
+        string rutaimglicencia = "";
+        public ChoferDatosEspecificos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerarContrato_Click(object sender, RoutedEventArgs e)
+        {
+            validar validaimg = new validar();
+            if (rutaimglicencia == "")
+                validaimg.show("No ha ingresado imagen de su licencia de conducir");
+            else if (panelUsuario.validar1(dateVencimientoLicencia.Text, txtVencimientoLicencia.Text, txtBNumerolicencia.Text, txtBNumerolicencia.Text))
+            {
+                String contratourl = generarContrato("Temuco");
+                
+                try
+                {
+                    MySqlConnection con = conexionDB.ObtenerConexion();
+                    String rutchofer = txtBRutChofer.Text+"-"+txtBdigVerificadorChofer.Text;
+                    
+
+                    string @path = System.IO.Directory.GetCurrentDirectory();
+                    path = path.Substring(0, path.Length - 9);
+                    path = path + "Images/fotoPerfil/";
+                    string filePath = path + System.IO.Path.GetFileName(rutaimglicencia);
+
+                    System.IO.File.Copy(rutaimglicencia, filePath, true);
+                    string sql = "INSERT INTO CHOFER (persona, numero_licencia, vencimiento_licencia, contrato, imagen_licencia) VALUES ";
+                    sql += "(?persona,?numerolicencia,?vencimientolicencia,?contrato,?imagenl)";
+
+                    MySqlCommand insertCommand = new MySqlCommand(sql, con);
+
+                    insertCommand.Parameters.Add("?persona", rutchofer);
+                    insertCommand.Parameters.Add("?numerolicencia", txtBNumerolicencia);
+                    insertCommand.Parameters.Add("?vencimientolicencia", dateVencimientoLicencia.Text);
+                    insertCommand.Parameters.Add("?contrato", contratourl.ToString());
+                    insertCommand.Parameters.Add("?imagenl", filePath.ToString());
+
+                    insertCommand.ExecuteNonQuery();
+                    con.Close();
+                    nuevoUsuario mensajeNuevo = new nuevoUsuario();
+                    mensajeNuevo.show(txtBNombre.Text);
+                }
+                catch (Exception ex) 
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                this.Close();
+            }
+            
+        }
+        private string generarContrato(string ciudad) 
+        {
+            DateTime today = DateTime.Today;
+            string[] textocontrato = new string[12];
+            textocontrato[0]= ciudad+", " + today.ToString(("d"));
+            textocontrato[1] = "Oficina SmarTravel";
+            textocontrato[2] = "Rudecindo Ortega 02950 salida norte, Temuco";
+            textocontrato[3] = "Representante legal Felipe Lagos Morapastene";
+
+            textocontrato[4] = "Se remite el presente contrato con efectos de  solicitar,  que  se  autorice  ";
+            textocontrato[5] = "la  suscripción  del  contrato  de  ejecución  de  servicios  que  adjunto  se  acompaña. ";
+            textocontrato[6] = "El   cual   ya  ha   sido   rubricado  por   el   interesado  a   realizarse  en  base  a  los ";
+            textocontrato[7] = "siguientes  datos:";
+            textocontrato[8] = "a)Personal  contratado :  Sr(a) " + txtBNombre.Text + "   Rut :  " + txtBRutChofer.Text + "-" + txtBdigVerificadorChofer.Text;
+            textocontrato[9] = "b)Funciones y o tareas a desempeñar: Chofer de buses urbanos y de encomienda";
+            textocontrato[10] = "c)Remuneraciones: XXX.XXX.XXX";
+            textocontrato[11] = "Duración del contrato:Indefinido";
+
+            PdfDocument pdf = new PdfDocument();
+            pdf.Info.Title = "";
+            PdfPage pdfPage = pdf.AddPage();
+            pdfPage.Orientation = PdfSharp.PageOrientation.Landscape;
+            pdfPage.Size = PdfSharp.PageSize.B5; 
+            XGraphics graph = XGraphics.FromPdfPage(pdfPage);
+            XImage image = XImage.FromFile("Timbre.jpg");
+            graph.DrawImage(image, 300, 350, 116, 118);
+            
+            XFont font = new XFont("Verdana", 12);
+            graph.DrawString(textocontrato[0], font, XBrushes.Black, new XRect(540, 40, 10, 0), XStringFormats.Default);
+            graph.DrawString(textocontrato[1], font, XBrushes.Black, new XRect(40, 100, 10, 0), XStringFormats.Default); //fecha e inicio de pagina
+            graph.DrawString(textocontrato[2], font, XBrushes.Black, new XRect(40, 115, 10, 0), XStringFormats.Default);
+            graph.DrawString(textocontrato[3], font, XBrushes.Black, new XRect(40, 130, 10, 0), XStringFormats.Default);
+            int rango = 200;
+            int ejey = 170;
+            for(int i=4;i<12;i++)
+            {
+                
+                graph.DrawString(textocontrato[i], font, XBrushes.Black, new XRect(ejey, rango, 10, 0), XStringFormats.Default); //contenido del contrato
+                rango += 18;
+                ejey = 100;
+            }
+            string pdfFilename = txtBRutChofer.Text + "-" + txtBdigVerificadorChofer.Text + ".pdf" ;
+            string path = @"C:\Contratos\";
+            pdf.Save(path + pdfFilename);
+
+            Process.Start(path + pdfFilename);
+            return path + pdfFilename;
+        }
+
+        private void btnCargarLicencia_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog open = new Microsoft.Win32.OpenFileDialog();
+            open.Filter = "Archivos jpg(*.jpg)|*.jpg";
+            open.Title = "Archivos Imagenes";
+       
+            if (open.ShowDialog() == true)
+            {
+                rutaimglicencia = open.FileName;
+            }
+            if (rutaimglicencia != "")
+            {
+                var uri = new Uri(rutaimglicencia);
+                var bitmap = new BitmapImage(uri);
+                imgLicenciaConducir.Source = bitmap;
+            }
+        }
+        
+
+    }
+}
---
+++ SmarTravel_Final/SmarTravel_Final/fichaChofer.xaml.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+using System.Data.Sql;
+using System.IO;
+
+namespace SmarTravel_Final
+{
+    /// <summary>
+    /// Lógica de interacción para fichaChofer.xaml
+    /// </summary>
+    public partial class fichaChofer : Window
+    {
+        public fichaChofer()
+        {
+            InitializeComponent();
+        }
+
+        public void llenarficha(string rut)
+        {
+            MySqlConnection con = conexionDB.ObtenerConexion();
+            try
+            {
+                string sql = "select p.nombre_completo , p.rut, c.numero_licencia, c.vencimiento_licencia, c.contrato, c.imagen_licencia ";
+                sql += "from persona as p inner join chofer as c on p.rut = c.persona where p.rut ='"+rut+"'";
+                Console.WriteLine(sql);
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+                MySqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+
+                    txtnombreChofer.Text = dr.GetValue(0).ToString();
+                    txtrutchofer.Text = dr.GetValue(1).ToString();
+                    txtNumerolicencia.Text = dr.GetValue(2).ToString();
+                    dateLicencia.Text = dr.GetValue(3).ToString();
+                    Console.WriteLine(dr.GetValue(5).ToString());
+                    var uri = new Uri(dr.GetValue(5).ToString());
+                    var bitmap = new BitmapImage(uri);
+                    imglicencia.Source = bitmap;
+
+                }
+                con.Close();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+
+        }
+
+        private void image1_MouseUp(object sender, RoutedEventArgs e) 
+        { 
+        }
+
+
+        private void datePicker2_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            txtnombreChofer.IsReadOnly = false;
+            txtnombreChofer.IsEnabled = true;
+            txtNumerolicencia.IsReadOnly = false;
+            txtNumerolicencia.IsEnabled = true;
+            dateLicencia.IsEnabled = true;
+        }
+
+        private void btnCargarfoto_Click(object sender, RoutedEventArgs e)
+        {
+            MySqlConnection con = conexionDB.ObtenerConexion();
+            Microsoft.Win32.OpenFileDialog open = new Microsoft.Win32.OpenFileDialog();
+            open.Filter = "Archivos jpg(*.jpg)|*.jpg";
+            open.Title = "Archivos Imagenes";
+            string ruta = "";
+
+            Nullable<bool> result = open.ShowDialog();
+            if (result == true)
+            {
+                ruta = open.FileName;
+
+            }
+
+            try
+            {
+                string path = System.IO.Directory.GetCurrentDirectory();
+                path = path.Substring(0, path.Length - 9);
+                path = path + "Images/fotoPerfil/";
+                string filePath = path + System.IO.Path.GetFileName(ruta);
+
+
+
+                string updateString = "UPDATE CHOFER SET imagen_licencia=?imagen WHERE persona=?rut";
+                MySqlCommand updateCommand = new MySqlCommand(updateString, con);
+                updateCommand.Parameters.Add("?imagen", filePath.ToString());
+                updateCommand.Parameters.Add("?rut", txtrutchofer.Text);
+                System.IO.File.Copy(ruta, filePath, true);
+                updateCommand.ExecuteNonQuery();
+
+                var uri = new Uri(filePath);
+                var bitmap = new BitmapImage(uri);
+                imglicencia.Source = bitmap;
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+        }
+
+        private void btnVerContrato_Click(object sender, RoutedEventArgs e)
+        {
+            string path = @"C:\Contratos\";
+            Process.Start(path + txtrutchofer.Text+".pdf");
+        }
+
+        private void btnIngresarVacaciones_Click(object sender, RoutedEventArgs e)
+        {
+            validar vali=new validar();
+            try
+            {
+                MySqlConnection con = conexionDB.ObtenerConexion();
+                string updateString = "UPDATE vacaciones_trabajadores SET inicio=?inicio, termino=?termino WHERE  persona=?rut";
+                MySqlCommand updateCommand = new MySqlCommand(updateString, con);
+                updateCommand.Parameters.Add("?inicio", dateinicio.Text);
+                updateCommand.Parameters.Add("?termino", dateTermino.Text);
+                updateCommand.Parameters.Add("?rut", txtrutchofer.Text);
+                updateCommand.ExecuteNonQuery();
+                con.Close();
+                vali.show("Se han ingresado con exito las vacaciones");
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnGuardarCambios_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtnombreChofer.IsReadOnly == false)
+            {
+                if (panelUsuario.validar1(txtnombreChofer.Text, lblNombre.Text, txtNumerolicencia.Text, lblNumerolicencia.Text))
+                {
+                    try
+                    {
+                        MySqlConnection con = conexionDB.ObtenerConexion();
+                        string updateStringP = "UPDATE PERSONA SET nombre_completo=?nombre WHERE rut='"+txtrutchofer.Text+"'";
+                        MySqlCommand updateCommand = new MySqlCommand(updateStringP, con);
+                        updateCommand.Parameters.Add("?nombre", txtnombreChofer.Text);               
+                        updateCommand.ExecuteNonQuery();
+                        con.Close();
+                        con.Open();
+                        string updateStringC = "UPDATE CHOFER SET numero_licencia=?nlicencia WHERE persona='"+txtrutchofer.Text+"'";
+                        MySqlCommand updateCommandC = new MySqlCommand(updateStringC, con);
+                        updateCommand.Parameters.Add("?nlicencia", txtNumerolicencia.Text); 
+                        updateCommand.ExecuteNonQuery();
+                        con.Close();
+
+                        nuevoUsuario update = new nuevoUsuario();
+                        update.textBlock1.Text = "Se ha actualizado correctamente al chofer en el sistema.";
+                        update.saludo.Visibility = Visibility.Hidden;
+                        update.mensajeUpdate.Visibility = Visibility.Visible;
+                        update.show("");
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        validar ventana = new validar();
+                        ventana.show(ex.Message);
+                    }
+         
+                }
+
+            }
+            else
+            {
+                validar error = new validar();
+                error.show("Seleccione el boton para editar el regitro");
+            }
+        }
+        
+    }
+}
